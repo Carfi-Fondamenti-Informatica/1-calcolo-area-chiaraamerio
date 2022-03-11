937f3f4a687ccc55928a72946afd5052e0115dfd
chiaraamerio <100305899+chiaraamerio@users.noreply.github.com>
2022-03-11 20:30:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,23 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
-
+  
+float a=0, b=0, c=0, area_triangolo=0, area_quadrato=0, area_rettangolo=0, area_trapezio=0;
+cout<<"inserire tre valori reali"<< endl;
+  
 cin >> a >> b >> c;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
+    area_triangolo = (a*b)/2;
+    area_quadrato = a*a;
+    area_rettangolo = a*b;
+    area_trapezio = ((a+b)*c)/2;
 
 
+    cout<<area_triangolo<< endl;
+    cout<<area_quadrato<< endl;
+    cout<<area_rettangolo<< endl;
+    cout<<area_trapezio<< endl;
+
+
+return 0; 
 }
